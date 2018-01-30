3198c18e20e5a262a539d484139e6078178a31fd
Robson Ramos <rosbonramosabrao@hotmail.com>
2018-01-30 20:19:30 -0200

    layout

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,6 +1,17 @@
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'mdbootstrap';
+import 'velocity-animate';
+import 'velocity';
+import 'custom-scrollbar';
+import 'jquery-visible';
+import 'ie10-viewport';
+
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
