9f41483d5e9894c60c16d85e11f88bb2dbcf46de
Flavio da Silva Alves <falves@britech.com.br>
2022-02-12 21:27:53 -0300

    feat: Invoice Controle performance

--- src/src/repositories/financeRepository.ts
+++ src/src/repositories/financeRepository.ts
@@ -8,58 +8,73 @@ import { EditInvoiceViewModel } from "../domain/editInvoiceViewModel";
 
 
 @autoinject
-export class FinanceRepository{
-    
+export class FinanceRepository {
 
-    api         : Rest;
+
+    api: Rest;
 
     constructor(private config: Config) {
         this.api = this.config.getEndpoint('apiAddress');
     }
 
-    getControls() : Promise<InvoiceControl[]>  {
+    getControlDates(): Promise<InvoiceControl[]> {
 
         return this.api
-                    .find('invoiceControl')
-                    .then( (result : Promise<InvoiceControl[]>) => {                 
-                        return result;
-                    })
-                    .catch( (e) => {
-                        console.log(e);
-                        return Promise.resolve(e.json().then( error => {
-                            throw error;
-                        }));
-                    });
+            .find('invoiceControl')
+            .then((result: Promise<InvoiceControl[]>) => {
+                return result;
+            })
+            .catch((e) => {
+                console.log(e);
+                return Promise.resolve(e.json().then(error => {
+                    throw error;
+                }));
+            });
     }
 
-    generateInvoices(selectedControl: InvoiceControl) : Promise<Invoice[]>{
+    getControls(control: InvoiceControl): Promise<InvoiceControl[]> {
+
         return this.api
-                    .post('invoice', { dateReference :  selectedControl.dateReference } )
-                    .then( (result : Promise<InvoiceControl[]>) => {                 
-                        return result;
-                    })
-                    .catch( (e) => {
-                        console.log(e);
-                        return Promise.resolve(e.json().then( error => {
-                            throw error;
-                        }));
-                    });
+            .find('invoiceControl/' + control.dateReference)
+            .then((result: Promise<InvoiceControl[]>) => {
+                return result;
+            })
+            .catch((e) => {
+                console.log(e);
+                return Promise.resolve(e.json().then(error => {
+                    throw error;
+                }));
+            });
     }
 
-    saveInvoice(invoice : EditInvoiceViewModel) : Promise<any> {
+    generateInvoices(selectedControl: InvoiceControl): Promise<Invoice[]> {
+        return this.api
+            .post('invoice', { dateReference: selectedControl.dateReference })
+            .then((result: Promise<InvoiceControl[]>) => {
+                return result;
+            })
+            .catch((e) => {
+                console.log(e);
+                return Promise.resolve(e.json().then(error => {
+                    throw error;
+                }));
+            });
+    }
+
+    saveInvoice(invoice: EditInvoiceViewModel): Promise<any> {
 
         debugger;
-        
+
         return this.api
-                    .post('editInvoice', invoice)
-                    .then( (result : Promise<InvoiceControl[]>) => {                 
-                        return result;
-                    })
-                    .catch( (e) => {
-                        console.log(e);
-                        return Promise.resolve(e.json().then( error => {
-                            throw error;
-                        }));
-                    });
-    } 
+            .post('editInvoice', invoice)
+            .then((result: Promise<InvoiceControl[]>) => {
+                return result;
+            })
+            .catch((e) => {
+                console.log(e);
+                return Promise.resolve(e.json().then(error => {
+                    throw error;
+                }));
+            });
+    }
 }
